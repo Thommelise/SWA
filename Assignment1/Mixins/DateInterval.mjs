e3c60d12c6580d64e503c24c76b7db881d0c67ac
Morten <thomsen91@outlook.com>
2021-09-22 14:45:21 +0200

    date

---
+++ Assignment1/Mixins/DateInterval.mjs
@@ -0,0 +1,17 @@
+function dateInterval(){
+    let dateString = Date().toString
+
+    function getFrom(data){
+        data = dateString
+        return dateString.
+    }
+
+    function getTo() {
+        return Date
+    }
+    return{ getFrom,getTo}
+}
+
+let test = dateInterval('7/13/2010')
+
+console.log(test.getFrom('7/13/2010'))
--- Assignment1/Mixins/WeatherHistoryMixin.mjs
+++ Assignment1/Mixins/WeatherHistoryMixin.mjs
@@ -6,6 +6,7 @@ function weatherHistory(value, time, place, type, unit) {
 
 const state = {value, time, place, type, unit}   
 const weatherDataFunction = weatherData(state)
+
 let list = []
     
 
@@ -18,26 +19,60 @@ function clearPlace(){
     delete state.place
 }
 
+function setType(data){
+    state.type = data
+}
+
+function clearType(){
+    delete state.type 
+}
+
 function add(){
     list.push(state)
 }
 
-function getAdd(){
+function getFilteredData(){
     return list
 }
 
+function convertToUSUints() {
+    if(unit === "MM") {
+        state.value = value * 1.8 + 32
+        state.unit = "Inch"
+    }
+}
+
+function convertToInternationalUnits() {
+    if(unit === "Inch") {
+        state.value = (value - 32) / 1.8
+        state.unit = "MM"
+    }
+}
     
 
-    return {setPlace,add,getAdd,clearPlace}
+    return {
+        ...weatherDataFunction,
+        setPlace,
+        clearPlace,
+        setType,
+        clearType,
+        add,
+        getFilteredData,
+         
+        convertToInternationalUnits,
+        convertToUSUints
+    }
 }
 
  
-let temp = weatherHistory(25.4,2000,"vidasda","rain","mm")
-let temp1 = weatherHistory(25.4,2000,"via","rain","mm")
+let temp = weatherHistory(25.4,2000,"visdd","rain","MM")
+let temp1 = weatherHistory(25.4,2000,"via","rain","MM")
+temp.convertToUSUints()
 temp.add()
 
 
-console.log(temp.getAdd()[0])
+
+console.log(temp.getFilteredData()[0].value)
 
 
 
