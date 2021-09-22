0f21b0a6bf8ce4834b895ec7953138010ba1b864
Thommelise <42830986+Thommelise@users.noreply.github.com>
2021-09-22 17:57:29 +0200

    Mixin fixed

--- Assignment1/Mixins/WeatherHistoryMixin.mjs
+++ Assignment1/Mixins/WeatherHistoryMixin.mjs
@@ -1,11 +1,12 @@
 import { weatherData } from "./WeatherDataMixin.mjs";
+import { dateInterval } from "./DateIntervalMixin.mjs";
 
 
+function weatherHistory(value, time, place, type, unit, dateFrom, dateTo) {
 
-function weatherHistory(value, time, place, type, unit) {
-
-const state = {value, time, place, type, unit}   
+const state = {value, time, place, type, unit, dateFrom, dateTo}   
 const weatherDataFunction = weatherData(state)
+const dateIntervalFunction = dateInterval(state)
 
 let list = []
     
@@ -27,6 +28,11 @@ function clearType(){
     delete state.type 
 }
 
+function setPeriodFilter(dateFrom, dateTo){
+    state.dateFrom = dateFrom
+    state.dateTo = dateTo
+}
+
 function add(){
     list.push(state)
 }
@@ -37,42 +43,45 @@ function getFilteredData(){
 
 function convertToUSUints() {
     if(unit === "MM") {
-        state.value = value * 1.8 + 32
+        state.value = value * 0.039370
         state.unit = "Inch"
     }
 }
 
 function convertToInternationalUnits() {
     if(unit === "Inch") {
-        state.value = (value - 32) / 1.8
+        state.value = value * 25.4
         state.unit = "MM"
     }
 }
     
 
     return {
+        ...dateIntervalFunction,
         ...weatherDataFunction,
+        setPeriodFilter,
         setPlace,
         clearPlace,
         setType,
         clearType,
         add,
         getFilteredData,
-         
         convertToInternationalUnits,
         convertToUSUints
     }
 }
 
  
-let temp = weatherHistory(25.4,2000,"visdd","rain","MM")
-let temp1 = weatherHistory(25.4,2000,"via","rain","MM")
+let temp = weatherHistory(25.4,2000,"visdd","rain","MM","07/06/2021","03/08/2021")
+let temp1 = weatherHistory(25.4,2000,"via","rain","MM","07/06/2021","03/08/2021")
 temp.convertToUSUints()
+temp.setPeriodFilter("22/06/3333","20/93/3929")
 temp.add()
 
 
 
-console.log(temp.getFilteredData()[0].value)
+
+console.log(temp.getFilteredData()[0].dateTo)
 
 
 
